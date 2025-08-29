7998962c54238f05f9ab5fec0651594213dca4b8
naim787 <naimmmmab@gmail.com>
2025-08-29 10:35:24 -0400

    Update: 8/29/2025, 10:35:24 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,7 +31,9 @@
         </div>
     </div>
 
-    <div class="w-full"></div>
+    <div class="w-full">
+      
+    </div>
     <!-- about -->
     <div class="w-full h-auto flex flex-row flex-wrap justify-evenly items-center gap-4">
       <!-- about -->
