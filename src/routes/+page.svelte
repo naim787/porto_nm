f2a7a290d871d81d0746b15d6e339705af15415d
naim787 <naimmmmab@gmail.com>
2025-08-29 08:02:28 -0400

    Update: 8/29/2025, 8:02:28 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -67,10 +67,10 @@
            <div class="w-full">
              <h1 class="">backend framework</h1>
             </div>
-            <!-- api routs -->
-            <h1 class="font-sans font-bold text-5xl m-2">EX</h1>
-            <img src="https://pbs.twimg.com/profile_images/1562960963359293446/rGjvMLR1_400x400.jpg" alt="nodjs" class="w-12">
-            <img src="https://gin-gonic.com/_astro/gin.D6H2T_2v_ZD2G7l.webp" alt="nodjs" class="w-12">
+          <!-- api routs -->
+          <h1 class="font-sans font-bold text-5xl m-2">EX</h1>
+          <img src="https://pbs.twimg.com/profile_images/1562960963359293446/rGjvMLR1_400x400.jpg" alt="nodjs" class="w-12">
+          <img src="https://gin-gonic.com/_astro/gin.D6H2T_2v_ZD2G7l.webp" alt="nodjs" class="w-12">
            <img src="https://www.svgrepo.com/show/368866/nuxtjs.svg" alt="nodjs" class="w-12">
            <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*5gMIBFvpNJivfyqv1ApwzA.png" alt="fiber" class="w-25">
            <img src="https://avatars.githubusercontent.com/u/119793569?s=200&v=4" alt="nodjs" class="w-12">
