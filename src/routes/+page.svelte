f6eb10f333b05bb77013571d902deaf11000ad2c
naim787 <naimmmmab@gmail.com>
2025-08-29 10:43:59 -0400

    Update: 8/29/2025, 10:43:58 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from "flowbite-svelte";
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Card, Button, Toggle } from "flowbite-svelte";
   import { ArrowRightOutline } from "flowbite-svelte-icons";
   let vCard = false;
 </script>
