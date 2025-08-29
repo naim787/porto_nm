cb39789bd0c3cbea9be9a2eac164d7ac35c2974f
naim787 <naimmmmab@gmail.com>
2025-08-29 08:04:27 -0400

    Update: 8/29/2025, 8:04:27 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -67,7 +67,7 @@
              <h1 class="">backend framework</h1>
            </div>
             <!-- api routs -->
-          <img src="https://www.svgrepo.com/show/354113/nextjs-icon.svg" alt="nodjs" class="w-12">
+           <img src="https://www.svgrepo.com/show/354113/nextjs-icon.svg" alt="nodjs" class="w-12">
            <h1 class="font-sans font-bold text-5xl m-2">EX</h1>
            <img src="https://pbs.twimg.com/profile_images/1562960963359293446/rGjvMLR1_400x400.jpg" alt="nodjs" class="w-12">
            <img src="https://gin-gonic.com/_astro/gin.D6H2T_2v_ZD2G7l.webp" alt="nodjs" class="w-12">
