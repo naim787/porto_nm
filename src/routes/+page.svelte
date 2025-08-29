401a2e3d1e9d14bccb6ca4aeed1396d3f3bdf3b1
naim787 <naimmmmab@gmail.com>
2025-08-29 04:54:04 -0400

    Update: 8/29/2025, 4:54:04 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -45,7 +45,7 @@
         <!-- skill -->
          <div class="w-[90vw] h-20 border-4 border-dashed border-gray-400 rounded-xl bg-white flex justify-evenly items-center">
            <img src="https://nodejs.org/static/logos/jsIconGreen.svg" alt="nodjs" class="w-12">
-           <img src="https://go.dev/blog/go-brand/Go-Logo/SVG/Go-Logo_Blue.svg" alt="nodjs" class="w-12">
+           <img src="https://go.dev/blog/go-brand/Go-Logo/SVG/Go-Logo_Blue.svg" alt="nodjs" class="w-">
            <img src="https://nodejs.org/static/logos/jsIconGreen.svg" alt="nodjs" class="w-12">
          </div>
     </div>
