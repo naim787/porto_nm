8524981266f6e1fdad5b2d3ed53c2b43999334f9
naim787 <naimmmmab@gmail.com>
2025-08-29 05:27:37 -0400

    Update: 8/29/2025, 5:27:37 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -62,6 +62,7 @@
            <!-- db -->
            https://www.svgrepo.com/show/374094/sqlite.svg
            https://www.svgrepo.com/show/353995/leveldb.svg
+           https://www.svgrepo.com/show/303251/mysql-logo.svg
          </div>
     </div>
 
