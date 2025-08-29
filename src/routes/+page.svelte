b3533b5e83859ff8c97596ea0f8e7086bb53ec45
naim787 <naimmmmab@gmail.com>
2025-08-29 03:59:24 -0400

    Update: 8/29/2025, 3:59:24 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,31 +3,27 @@
 </script>
 
 <Navbar class="fixed bg-white border border-b-1 border-gray-200">
-  <NavBrand href="/">
-    <div class="flex items-end">
-        <h1 class="mcpe-font text-4xl font-bold p-2">N</h1>
-    </div>
-  </NavBrand>
+  <NavBrand href="/" class="mcpe-font text-5xl font-bold p-2">N</NavBrand>
   <NavHamburger class="bg-gray-300"/>
   <NavUl class="h-15 bg-white rounded-md">
     <NavLi href="/">Home</NavLi>
-    <NavLi href="/docs/components/navbar">Navbar</NavLi>
-    <NavLi href="/docs/components/accordion">Accordion</NavLi>
-    <NavLi href="/docs/components/alert">Alert</NavLi>
+    <NavLi href="/">Navbar</NavLi>
+    <NavLi href="/">Accordion</NavLi>
+    <NavLi href="/">Alert</NavLi>
     <NavLi href="/docs/components/avatar">Avatar</NavLi>
   </NavUl>
 </Navbar>
 
 
-<div class="w-[100vw] h-auto flex flex-col bg-[radial-gradient(#BDBDBD_1px,transparent_0)] [background-size:30px_30px] overflow-x-hidden">
+<div class="w-[100vw] h-auto flex flex-col justify-center items-center bg-[radial-gradient(#BDBDBD_1px,transparent_0)] [background-size:30px_30px] overflow-x-hidden">
 
     <!-- main -->
-    <div class="w-full h-[100vh] flex justify-center items-center p-2">
+    <div class="w-[90vw] h-[100vh] flex justify-center items-center">
         <div class="w-full h-auto border-[1px] bg-white border-dashed rounded-xl border-gray-300 shadow-2xl flex-col 
         p-4 xl:shadow-none xl:bg-transparent xl:border-none">
            <div class="mcpe-font text-green-700 font-vold text-[2vh]">2025</div>
            <div class="mcpe-font text-[9vh] font-bold">NAIM ABDULLAH</div>
-           <div class="flex w-full bg-red-500 gap-2 font-bold">
+           <div class="flex w-full gap-2 font-bold">
                <p class="p-2 bg-green-500/50 text-green-900 rounded-xl">NODEJS</p>
                <p class="p-2 bg-blue-500/50 text-blue-900 rounded-md">GO</p>
                <p class="p-2 bg-gray-300 rounded-md">BUN</p>
