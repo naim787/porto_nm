bff20cdc9cbd5373e4e617ccea95948028b34f12
naim787 <naimmmmab@gmail.com>
2025-08-29 05:45:11 -0400

    Update: 8/29/2025, 5:45:11 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -65,7 +65,6 @@
            
            <img src=" https://www.svgrepo.com/show/303251/mysql-logo.svg" alt="nodjs" class="w-20">
            <img src=" https://www.svgrepo.com/show/303251/mysql-logo.svg" alt="nodjs" class="w-20">
-           https://www.svgrepo.com/show/373845/mongo.svg
            https://miro.medium.com/v2/resize:fit:470/format:webp/1*T1txuZBh1-Upx9c_po1bpA.png
 
 
