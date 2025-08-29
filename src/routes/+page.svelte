8e7c13106c4c62e5f14a15541d3003a9bdb6ac07
naim787 <naimmmmab@gmail.com>
2025-08-29 05:51:49 -0400

    Update: 8/29/2025, 5:51:49 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -60,6 +60,7 @@
            <img src="https://www.svgrepo.com/show/305719/apachekafka.svg" alt="nodjs" class="w-12">
            <!-- api routs -->
            <img src="https://pbs.twimg.com/profile_images/1562960963359293446/rGjvMLR1_400x400.jpg" alt="nodjs" class="w-12">
+           https://gin-gonic.com/_astro/gin.D6H2T_2v_ZD2G7l.webp
 
            <!-- db -->
            <img src="https://www.svgrepo.com/show/374094/sqlite.svg" alt="nodjs" class="w-12">
