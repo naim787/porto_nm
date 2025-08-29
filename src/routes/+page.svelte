c2549be26456310be5eccbf26fec080321468d0b
naim787 <naimmmmab@gmail.com>
2025-08-29 05:26:18 -0400

    Update: 8/29/2025, 5:26:18 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -58,6 +58,7 @@
            <img src="https://avatars.githubusercontent.com/u/119793569?s=200&v=4" alt="nodjs" class="w-12">
            <img src="https://astro.build/assets/press/astro-icon-light-gradient.svg" alt="nodjs" class="w-12 bg-black p-2 rounded-xl">
            https://www.svgrepo.com/show/305719/apachekafka.svg
+           https://www.svgrepo.com/show/353995/leveldb.svg
          </div>
     </div>
 
