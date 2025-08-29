dfac3409fc6c750fedf80ce365b6cc9a51ee3f78
naim787 <naimmmmab@gmail.com>
2025-08-29 04:51:28 -0400

    Update: 8/29/2025, 4:51:28 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -43,7 +43,7 @@
         </div>
 
         <!-- skill -->
-         <div class="w-[90vw] h-20 border-4 border-dashed border-gray-400 rounded-xl bg-white flex ">
+         <div class="w-[90vw] h-20 border-4 border-dashed border-gray-400 rounded-xl bg-white flex">
 
          </div>
     </div>
