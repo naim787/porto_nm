afd3471288db24bf35783fd231118c1648425cb6
naim787 <naimmmmab@gmail.com>
2025-08-29 10:15:36 -0400

    Update: 8/29/2025, 10:15:36 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -93,7 +93,7 @@
            <img src="https://www.svgrepo.com/show/373845/mongo.svg" alt="nodjs" class="w-20">
            <img src="https://miro.medium.com/v2/resize:fit:470/format:webp/1*T1txuZBh1-Upx9c_po1bpA.png" alt="nodjs" class="w-15">
            <div class="w-full">
-             <h1 class="">oprasig system</h1>
+             <h1 class="">devops</h1>
            </div>
            <img src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/97_Docker_logo_logos-512.png" alt="nodjs" class="w-15">
            <div class="w-full">
