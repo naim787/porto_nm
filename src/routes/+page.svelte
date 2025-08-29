431f531f4a7b1eaacc295c603ff27954505e70d0
naim787 <naimmmmab@gmail.com>
2025-08-29 08:02:14 -0400

    Update: 8/29/2025, 8:02:14 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -70,14 +70,14 @@
            <img src="https://www.svgrepo.com/show/368866/nuxtjs.svg" alt="nodjs" class="w-12">
            <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*5gMIBFvpNJivfyqv1ApwzA.png" alt="fiber" class="w-25">
            <img src="https://avatars.githubusercontent.com/u/119793569?s=200&v=4" alt="nodjs" class="w-12">
-           <img src="https://www.svgrepo.com/show/305719/apachekafka.svg" alt="nodjs" class="w-12">
            <!-- api routs -->
-            <h1 class="font-sans font-bold text-5xl m-2">EX</h1>
+           <h1 class="font-sans font-bold text-5xl m-2">EX</h1>
            <img src="https://pbs.twimg.com/profile_images/1562960963359293446/rGjvMLR1_400x400.jpg" alt="nodjs" class="w-12">
            <img src="https://gin-gonic.com/_astro/gin.D6H2T_2v_ZD2G7l.webp" alt="nodjs" class="w-12">
            <img src="https://avatars.githubusercontent.com/u/28507035?s=48&v=4" alt="nodjs" class="w-12">
            
            <!-- unitest api -->
+           <img src="https://www.svgrepo.com/show/305719/apachekafka.svg" alt="nodjs" class="w-12">
            <img src="https://upload.wikimedia.org/wikipedia/commons/6/60/K6_software_logo.svg" alt="nodjs" class="w-12">
 
            <!-- db -->
