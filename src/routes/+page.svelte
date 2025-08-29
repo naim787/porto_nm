87749f542466d00b0623a38194c6259be417fb6c
naim787 <naimmmmab@gmail.com>
2025-08-29 04:19:34 -0400

    Update: 8/29/2025, 4:19:34 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -33,7 +33,7 @@
 
     <!-- about -->
     <div class="w-full h-[100vh] flex flex-col items-center">
-        <div class="w-80 h-100 bg-white rounded-xl border-4 border-dashed relative p-3">
+        <div class="w-80 h-100 bg-white rounded-xl border-4 border-dashed relative p-2">
           <h1 class="text-8xl absolute left-[40%] top-[-22%] transform origin-center rotate-180 mcpe-font">^</h1>
           <h1 class="text-3xl">About </h1>
         </div>
