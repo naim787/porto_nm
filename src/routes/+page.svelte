133447279b7445c92992f8cf00836bcb36439baf
naim787 <naimmmmab@gmail.com>
2025-08-29 05:17:49 -0400

    Update: 8/29/2025, 5:17:49 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -54,7 +54,7 @@
            <img src="https://www.svgrepo.com/show/303494/vue-9-logo.svg" alt="nodjs" class="w-12">
            <img src="https://www.svgrepo.com/show/368866/nuxtjs.svg" alt="nodjs" class="w-12">
            <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*5gMIBFvpNJivfyqv1ApwzA.png" alt="nodjs" class="w-12">
-           <img src="https://avatars.githubusercontent.com/u/119793569?s=200&v=4" alt="nodjs" class="w-12">
+           <img src="https://avatars.githubusercontent.com/u/119793569?s=200&v=4" alt="nodjs" class="w-12"> <img src="https://avatars.githubusercontent.com/u/119793569?s=200&v=4" alt="nodjs" class="w-12">
            https://astro.build/assets/press/astro-icon-light-gradient.svg
 
          </div>
