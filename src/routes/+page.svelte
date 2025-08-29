13596009ec51ee2464426249b609aa46a225674b
naim787 <naimmmmab@gmail.com>
2025-08-29 10:42:36 -0400

    Update: 8/29/2025, 10:42:36 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -110,7 +110,18 @@
 
   <!-- projects -->
   <div class="w-full h-[100vh] flex justify-evenly mt-20">
-    <div class="w-110 h-105 border-3 border-dashed border-gray-400 rounded-xl bg-white"></div>
+    <div class="space-y-4">
+  <Card img="/images/image-1.webp" reverse={vCard}>
+    <div class="m-6">
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+      <p class="mb-3 leading-tight font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+      <Button class="w-40">
+        Read more <ArrowRightOutline class="ms-2 h-6 w-6 text-white" />
+      </Button>
+    </div>
+  </Card>
+  <Toggle bind:checked={vCard} class="italic dark:text-gray-500">Reverse</Toggle>
+</div>
   </div>
 
 </div>
