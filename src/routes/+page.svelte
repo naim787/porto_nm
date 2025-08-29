c52dc7141da2f129f9ea2165fe2ebcdd0ed30e54
naim787 <naimmmmab@gmail.com>
2025-08-29 07:59:08 -0400

    Update: 8/29/2025, 7:59:08 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -55,7 +55,7 @@
            <img src="https://nodejs.org/static/logos/jsIconGreen.svg" alt="nodjs" class="w-12">
            <img src="https://go.dev/blog/go-brand/Go-Logo/SVG/Go-Logo_Blue.svg" alt="nodjs" class="w-30">
            <img src="https://bun.com/logo.svg" alt="nodjs" class="w-12">
-            <div class="w-full">
+          <div class="w-full">
             <h1 class="">Language & Runtime</h1>
           </div>
            <img src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Svelte_Logo.svg" alt="nodjs" class="w-12">
