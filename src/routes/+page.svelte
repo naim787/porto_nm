93c9c8ebaf24613a1e70f2d0201f9bcf5abc128f
naim787 <naimmmmab@gmail.com>
2025-08-29 08:14:43 -0400

    Update: 8/29/2025, 8:14:43 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -44,7 +44,7 @@
 
         <!-- skill -->
          <div class="w-115 h-auto border-4 border-dashed border-gray-400 rounded-xl flex justify-start items-center flex-wrap p-3 relative">
-          <h1 class="text-8xl absolute left-[40%] top-[-16%] transform origin-center rotate-180 mcpe-font">^</h1>
+          <h1 class="text-8xl absolute left-[40%] top-[-15%] transform origin-center rotate-180 mcpe-font">^</h1>
           <div class="w-full">
             <h1 class="text-3xl font-bold">SKILL ISSUE</h1>
           </div> 
