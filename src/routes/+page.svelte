2ea9b5bcd4b894f4722f11657658471c5ba47906
naim787 <naimmmmab@gmail.com>
2025-08-29 05:07:31 -0400

    Update: 8/29/2025, 5:07:31 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -51,6 +51,7 @@
            <img src="https://www.svgrepo.com/show/452092/react.svg" alt="nodjs" class="w-12">
            <img src="https://www.svgrepo.com/show/354113/nextjs-icon.svg" alt="nodjs" class="w-12">
            <img src="https://www.svgrepo.com/show/354521/vitejs.svg" alt="nodjs" class="w-12">
+           <img src="https://www.svgrepo.com/show/354521/vitejs.svg" alt="nodjs" class="w-12">
            https://www.svgrepo.com/show/303494/vue-9-logo.svg
 
          </div>
