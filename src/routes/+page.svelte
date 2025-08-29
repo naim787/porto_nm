6fe4cd6b83da2e2062331d1393bb8752cff4edbd
naim787 <naimmmmab@gmail.com>
2025-08-29 10:22:56 -0400

    Update: 8/29/2025, 10:22:56 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -106,7 +106,7 @@
 
 
     <!-- projects -->
-    <div class="w-[90vw] h-[100vh] b">
+    <div class="w-[90vw] h-[100vh] bg-red">
       <div class="w-80 h-96 border-3 border-dashed"></div>
     </div>
 
