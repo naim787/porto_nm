6eb9f959c2098182ff5d1310f4f63d55f9ad6d3f
naim787 <naimmmmab@gmail.com>
2025-08-29 04:51:50 -0400

    Update: 8/29/2025, 4:51:50 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -44,7 +44,7 @@
 
         <!-- skill -->
          <div class="w-[90vw] h-20 border-4 border-dashed border-gray-400 rounded-xl bg-white flex justify-evenly items-center">
-           <img src="" alt="" class="">
+           <img src="https://nodejs.org/static/logos/jsIconGreen.svg" alt="" class="">
          </div>
     </div>
 
