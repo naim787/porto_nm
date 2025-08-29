229d282ccc4828f079e6078810ba82de85b8b8ca
naim787 <naimmmmab@gmail.com>
2025-08-29 04:55:36 -0400

    Update: 8/29/2025, 4:55:36 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -47,6 +47,7 @@
            <img src="https://nodejs.org/static/logos/jsIconGreen.svg" alt="nodjs" class="w-12">
            <img src="https://go.dev/blog/go-brand/Go-Logo/SVG/Go-Logo_Blue.svg" alt="nodjs" class="w-30">
            <img src="https://nodejs.org/static/logos/jsIconGreen.svg" alt="nodjs" class="w-12">
+           
          </div>
     </div>
 
