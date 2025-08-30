1d785cf1c9e359eb918169490ce2c871269a5566
naim787 <naimmmmab@gmail.com>
2025-08-30 07:38:24 -0400

    Update: 8/30/2025, 7:38:24 AM

--- src/lib/components/Fot.svelte
+++ src/lib/components/Fot.svelte
@@ -5,7 +5,7 @@
 <Footer footerType="socialmedia">
   <div class="md:flex md:justify-between">
     <div class="mb-6 md:mb-0">
-      <FooterBrand href="https://flowbite.com" src="/" alt="Flowbite Logo" name="Flowbite" />
+      <FooterBrand href="https://flowbite.com" src="" alt="Flowbite Logo" name="Flowbite" />
     </div>
     <div class="grid grid-cols-2 gap-8 sm:grid-cols-3 sm:gap-6">
       <div>
