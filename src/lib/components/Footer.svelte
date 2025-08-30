3a8da6074b0be0c719c9fb16a618432ad34d02e1
naim787 <naimmmmab@gmail.com>
2025-08-30 07:33:23 -0400

    Update: 8/30/2025, 7:33:23 AM

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -33,6 +33,4 @@
     </div>
   </div>
   <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8 dark:border-gray-700" />
-  <div class="sm:flex sm:items-center sm:justify-between">
-  </div>
 </Footer>
