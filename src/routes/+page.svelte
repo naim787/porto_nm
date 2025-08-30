4fdc1fdea1a16707f1226c9a82426e7623b0a069
naim787 <naimmmmab@gmail.com>
2025-08-30 07:34:43 -0400

    Update: 8/30/2025, 7:34:43 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-  import Footer from "$lib/components/Footer.svelte";
+  import Footer from "$lib/components/Fot.svelte";
   import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Card, Button, Toggle, Li, List, Heading } from "flowbite-svelte";
 </script>
 
