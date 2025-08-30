b3de2de23a45cb0c0a9b2bce810ee83036c9240d
naim787 <naimmmmab@gmail.com>
2025-08-30 07:41:05 -0400

    Update: 8/30/2025, 7:41:05 AM

--- src/lib/components/Fot.svelte
+++ src/lib/components/Fot.svelte
@@ -7,7 +7,7 @@
     <div class="mb-6 md:mb-0">
       <!-- <h1 class="text-8xl mcpe-font">N</h1> -->
     </div>
-    <div class="flex ">
+    <div class="flex flex-col">
       <div>
         <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
         <FooterLinkGroup>
