1d6b38f0e6b75975b900099e8504892c1886a772
naim787 <naimmmmab@gmail.com>
2025-08-30 07:24:11 -0400

    Update: 8/30/2025, 7:24:11 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -166,7 +166,7 @@
       EMAIL : <a href="/" class="font-serif">naimmmmab@gmail.com</a>
     </Li>
     <Li>
-      GITHUB :   <a href="https://github.com/naim787" class="font-serif">naim787</a>
+      GITHUB :  <a href="https://github.com/naim787" class="font-serif">naim787</a>
     </Li>
     <Li>
       YOU TOUBE :
