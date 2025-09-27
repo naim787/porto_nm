e8a5bfcfb51360d2719cc46b0cd6c31a3e17f75c
naim787 <naimmmmab@gmail.com>
2025-09-27 11:35:40 -0400

    Update: 9/27/2025, 11:35:40 AM

--- src/lib/components/Projects.svelte
+++ src/lib/components/Projects.svelte
@@ -5,7 +5,7 @@
   let dataProjects = [
     {
         src : "https://raw.githubusercontent.com/naim787/zkfs/refs/heads/main/src/lib/assets/favicon.webp",
-        cap : "ZKFS adalah sebuah app encripsi pasword dan data sensitif",
+        cap : "ZKFS adalah sebuah app encripsi data sensitif",
         stack : [
             {
                 src : "",
@@ -24,7 +24,7 @@
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{data.cap}</h5>
             </div>
         </Card>
-        <h1 class="absolute top-[55%] left-[40%] bg-linear-to-r from-cyan-300 to-white bg-clip-text text-5xl font-extrabold text-transparent">ZKFS</h1>
+        <h1 class="absolute top-[60%] left-[40%] bg-linear-to-r from-cyan-300 to-white bg-clip-text text-5xl font-extrabold text-transparent">ZKFS</h1>
     </div>
     {/each}
   </div> 
