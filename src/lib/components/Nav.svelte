5d293c14eec19bdb0a535b1af6c23aec095d74f4
naim787 <naimmmmab@gmail.com>
2025-10-29 20:31:28 -0400

    Update: 10/29/2025, 8:31:28 PM

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -3,17 +3,17 @@
   import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from "flowbite-svelte";
 </script>
 
-<Navbar class="fixed top-0 left-0 bg-black/50 backdrop-blur-2xl">
+<Navbar class="fixed top-0 left-0 bg-black/70 backdrop-blur-2xl text-white z-50">
   <NavBrand href="/">
     <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
     <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
   </NavBrand>
   <NavHamburger />
-  <NavUl >
-    <NavLi href="/">Home</NavLi>
-    <NavLi href="/docs/components/navbar">Navbar</NavLi>
-    <NavLi href="/docs/components/accordion">Accordion</NavLi>
-    <NavLi href="/docs/components/alert">Alert</NavLi>
-    <NavLi href="/docs/components/avatar">Avatar</NavLi>
+  <NavUl class="text-white">
+    <NavLi class="text-white" href="/">Home</NavLi>
+    <NavLi class="text-white" href="/docs/components/navbar">Navbar</NavLi>
+    <NavLi class="text-white" href="/docs/components/accordion">Accordion</NavLi>
+    <NavLi class="text-white" href="/docs/components/alert">Alert</NavLi>
+    <NavLi class="text-white" href="/docs/components/avatar">Avatar</NavLi>
   </NavUl>
 </Navbar>
